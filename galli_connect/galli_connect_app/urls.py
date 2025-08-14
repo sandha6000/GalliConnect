3f35f2c69a53944626bd0b66361976b2aaa1b430
Prakruthi <prakruthi1026@gmail.com>
2025-08-14 18:25:08 +0530

    update signup and login url paths

--- galli_connect/galli_connect_app/urls.py
+++ galli_connect/galli_connect_app/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('', views.homepage, name='homepage'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login')
 ]
 
 
